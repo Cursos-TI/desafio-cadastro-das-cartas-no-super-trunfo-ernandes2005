0d7a3f920cee26f55880e7dbfe4931ef4441d87c
ernandes2005 <ernandesjunior1278@gmail.com>
2025-03-24 22:47:55 +0000

    super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
 
 int main(){ 
-    printf ("ola mundo");
-    return 0;
+
+    //variaveis da carta 1//
+    char estado1, codigo1[20], cidade1[50];
+    int populacao1, pontosTuristicos1;
+    float pib1, area1;
+
+    //variaveis da carta 2//
+    char estado2, codigo2[20], cidade2[50];
+    int populacao2, pontosturisticos2;
+    float pib2, area2;
+
+
+    //**entrada de dados da carta 1**//
+    printf ("**insira os dados da carta 1**:\n");
+    printf ("estado:\n");
+    scanf (" %c", &estado1);
+
+    printf ("codigo:\n");
+    scanf (" %19s", codigo1);
+
+    printf ("cidade:\n");
+    scanf ("%49s", cidade1);
+
+    printf ("populacao:\n");
+    scanf ("%d", &populacao1);
+
+    printf ("area:\n");
+    scanf ("%f", &area1);
+
+    printf ("pib:\n");
+    scanf ("%f", &pib1);
+
+    printf ("pontosturisticos:\n");
+    scanf ("%d", &pontosTuristicos1);
+
+    //**entrada de dados da carta 2**//
+    printf ("insira os dados da carta 2: \n");
+    printf ("estado:\n");
+    scanf (" %c", &estado2);
+
+    printf ("codigo:\n");
+    scanf ("%19s", codigo2);
+
+    printf ("cidade:\n");
+    scanf ("%49s", cidade2);
+
+    printf ("populacao:\n");
+    scanf ("%d",&populacao2);
+
+    printf ("area:\n");
+    scanf ("%f", &area2);
+
+    printf ("pib:\n");
+    scanf ("%f", &pib2);
+
+    printf ("pontosturisticos:\n");
+    scanf ("%d",&pontosturisticos2);
+
+return 0;
+
 
 }
