7173488ebfd40a945f4698514095814628c197f3
ernandes2005 <ernandesjunior1278@gmail.com>
2025-03-31 23:12:13 +0000

    super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,12 +6,12 @@ int main(){
     //variaveis da carta 1//
     char estado1, codigo1[20], cidade1[50];
     int populacao1, pontosTuristicos1;
-    float pib1, area1;
+    float pib1, area1, densidade1, PibPerCapita1;
 
     //variaveis da carta 2//
     char estado2, codigo2[20], cidade2[50];
     int populacao2, pontosTuristicos2;
-    float pib2, area2;
+    float pib2, area2, densidade2, PibPerCapita2;
 
 
     //**entrada de dados da carta 1**//
@@ -37,6 +37,16 @@ int main(){
     printf ("pontosturisticos:\n");
     scanf ("%d", &pontosTuristicos1);
 
+    // Cálculo da densidade populacional 1//
+    densidade1 = populacao1 / (area1 + (area1 == 0));
+    // Imprimindo o resultado da densidade populacional//
+    printf("Densidade Populacional 1: %.2f hab/km²\n", densidade1);
+
+    //calculo do pib per capita 1//
+    PibPerCapita1 = pib1 / (populacao1 + (populacao1 == 0));
+    //imprimir resultado pib per capita 1//
+    printf("PIB Per Capita 1: %.2f\n", PibPerCapita1);
+
     //**entrada de dados da carta 2**//
     printf ("insira os dados da carta 2: \n");
     printf ("estado:\n");
@@ -59,6 +69,16 @@ int main(){
 
     printf ("pontosturisticos:\n");
     scanf ("%d",&pontosTuristicos2);
+
+    // Cálculo da densidade populacional 2//
+    densidade2 = populacao2 / area2 + (area2 == 0);
+    // Imprimindo o resultado da densidade populacional
+    printf("Densidade Populacional 2: %.2f hab/km²\n", densidade2);
+
+    //calculo do pib per capita 2//
+    PibPerCapita2 = pib2 / (populacao2 + (populacao2 == 0));
+    //imprimir resultado pib per capita 2//
+    printf("PIB Per Capita 2: %.2f\n", PibPerCapita2);
 
 return 0;
 
